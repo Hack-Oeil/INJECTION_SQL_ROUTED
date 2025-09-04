1ec4c647584328b881d061102b2f96955a8165dd
cyrhades <cyrhades76@gmail.com>
2025-09-04 14:39:12 +0200

    test correction encodage UTF8

--- .docker/db/init.sql/products.sql
+++ .docker/db/init.sql/products.sql
@@ -1,4 +1,16 @@
-﻿create table product (
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+-- --------------------------------------------------------
+
+create table product (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	title VARCHAR(255),
 	description TEXT,
@@ -6,8 +18,9 @@
 	stock INT,
 	picture VARCHAR(255),
 	category INT
-);
+) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+  
 insert into product (id, title, description, price, stock, picture, category) values (1, 'Polo Original L.12.12 coton petit piqué', "Le t-shirt décontracté à col rond est un essentiel de la garde-robe décontractée. Avec son col rond classique et sa coupe confortable, ce t-shirt offre un look simple et polyvalent. Fabriqué à partir de matériaux de qualité, il est doux au toucher et agréable à porter. Ce t-shirt est un choix idéal pour les journées détente, les sorties entre amis ou même pour une tenue de tous les jours. Disponible dans une variété de couleurs et de motifs, ce t-shirt vous permet d'exprimer votre style personnel de manière décontractée et confortable.", 110.00, 18, 'product_1.jpg', 1);
 insert into product (id, title, description, price, stock, picture, category) values (2, 'T-shirt décontracté à col rond', "Le polo Original L.12.12 en coton petit piqué incarne le chic intemporel. Ce polo emblématique porte le nom du créateur du polo, René Lacoste, et est conçu avec une grande attention aux détails. Fabriqué à partir de coton de haute qualité, il offre une sensation de douceur et de confort tout au long de la journée. Doté d'un col classique, de manches courtes et du logo crocodile emblématique de Lacoste, ce polo est un symbole de style décontracté et élégant. Que ce soit pour une journée au bureau, une sortie en ville ou une partie de golf, le Polo Original L.12.12 est un choix qui ne se démode jamais.", 29.99, 21, 'product_2.jpg', 1);
 insert into product (id, title, description, price, stock, picture, category) values (3, 'Tee Shirt Charing Noir', "Le tee-shirt Charing Noir est synonyme de simplicité et d'élégance. Sa couleur noire intemporelle en fait un choix polyvalent pour diverses occasions. Conçu pour un confort maximal, ce tee-shirt offre une coupe ajustée qui met en valeur votre silhouette. Que ce soit pour une réunion professionnelle ou une soirée décontractée entre amis, le tee-shirt Charing Noir s'adapte à toutes les situations. Affichez un look élégant et classique avec ce tee-shirt noir.", 29.99, 23, 'product_3.jpg', 1);
@@ -23,7 +36,7 @@ insert into product (id, title, description, price, stock, picture, category) va
 insert into product (id, title, description, price, stock, picture, category) values (13, 'T-shirt basique', "", 28.99, 18, 'product_13.jpg', 2);
 insert into product (id, title, description, price, stock, picture, category) values (14, 'MIT MITTELNAHT - Pantalon classique', "", 80.00, 18, 'product_14.jpg', 2);
 insert into product (id, title, description, price, stock, picture, category) values (15, 'Pyjama basketball garçon - vert sapin', "", 20.00, 18, 'product_15.jpg', 3);
-insert into product (id, title, description, price, stock, picture, category) values (16, 'T-shirt message en relief fille - vert foncé', "", 21.99, 18, 'product_16.jpg', 3);
+insert into product (id, title, description, price, stock, picture, category) values (16, 'T-shirt message en relief fille - vert foncéé', "", 21.99, 18, 'product_16.jpg', 3);
 insert into product (id, title, description, price, stock, picture, category) values (17, 'Pyjama fille Harry Potter® - rose pâle/imprimé carreaux', "", 39.99, 18, 'product_17.jpg', 3);
 insert into product (id, title, description, price, stock, picture, category) values (18, 'Robe en molleton fleurs pop fille - bleu grisé', "", 24.99, 18, 'product_18.jpg', 3);
 insert into product (id, title, description, price, stock, picture, category) values (19, 'Sweat zippé à capuche motif "Team" sport fille - vert', "", 29.99, 18, 'product_19.jpg', 3);
--- .docker/db/init.sql/users.sql
+++ .docker/db/init.sql/users.sql
@@ -1,10 +1,23 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+-- --------------------------------------------------------
+
 create table user (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	firstname VARCHAR(50),
 	lastname VARCHAR(50),
 	email VARCHAR(50),
 	password VARCHAR(50)
-);
+) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 insert into user (id, firstname, lastname, email, password) values (1, 'Bride', 'Leckie', 'bleckie0@nsw.gov.au', '4d1e172265d9037a1de3d6d9c11948ba36871b79');
 insert into user (id, firstname, lastname, email, password) values (2, 'Dalston', 'Haysham', 'dhaysham1@rakuten.co.jp', '7ebfed27ddc491277c348bf505028e2f6d3784cf');
 insert into user (id, firstname, lastname, email, password) values (3, 'Admin', '-', 'admin@boutik.com', 'd8cd10b920dcbdb5163ca0185e402357bc27c265');
